b411bacafdc8ec78f8e15c1b32d8de7999a006a9
Narinder Gupta <narinder.gupta@gmail.com>
2022-09-02 19:35:24 -0400

    commit 6

---
+++ Selenium_Sept_Prac/src/test/java/Day2_3/ArithmaticOperators.java
@@ -0,0 +1,21 @@
+package Day2_3;
+
+public class ArithmaticOperators {
+
+	public static void main(String[] args) {
+		// //Arithmetic operators   + - * / % 
+		
+		int a,b,c;
+		a=8; 
+		b=4;
+		int result = a+b;
+		System.out.println(result);
+		System.out.println(b%a);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+	
+
+	}
+
+}
