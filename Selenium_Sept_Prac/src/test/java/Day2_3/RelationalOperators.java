edd651eb6fd5b273baf73ddb140319402cd788f1
Narinder Gupta <narinder.gupta@gmail.com>
2022-09-02 19:53:56 -0400

    ng commit 8

---
+++ Selenium_Sept_Prac/src/test/java/Day2_3/RelationalOperators.java
@@ -0,0 +1,23 @@
+package Day2_3;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// //Relational/comparison operators  >  >=  <  <=  !=  == 
+		// returns true/false ( boolean) 
+		
+		int a=34;
+		int b=55;
+		boolean result;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a>=b);
+		System.out.println(b>=a);
+		System.out.println(a!=b);
+			
+		}
+ 
+	}
+
+
