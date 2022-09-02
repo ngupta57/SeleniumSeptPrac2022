b2925803b4edb85cd3f278fd7e0e119effe4d2f0
Narinder Gupta <narinder.gupta@gmail.com>
2022-09-01 21:58:53 -0400

    third commit

---
+++ Selenium_Sept_Prac/src/test/java/Day1/DataTypeDemo2.java
@@ -0,0 +1,24 @@
+package Day1;
+
+public class DataTypeDemo2 {
+
+	public static void main(String[] args) {
+		/*
+		 * // int a=100, b=200;
+		 * 
+		 * System.out.println("The value of a is:"+ a);* System.out.println("The value of b is:"+ b); long x=2312312317656L; System.out.println(x);
+		 * 
+		 * float d=150.578f; System.out.println(d);* byte person_age=30; // byte, short, int, long System.out.println("the person age is:"+person_age);
+		 *  
+		 * float item_price=150.55f; System.out.println("item price is:"+item_price);* char stu_grad='B'; System.out.println("Student grade is:"+ stu_grad);
+		 * * boolean status=true; System.out.println("status is:"+ status); * * String person_name="John Kenedy";
+		 * System.out.println("Name of the person is:"+ person_name);
+		 */
+		
+		int x=100,y=200;
+		System.out.println(x+y);
+		
+		
+	}
+
+}
