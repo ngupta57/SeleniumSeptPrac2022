b57f92cfa882aed88275a29b42c08e34047517b6
Narinder Gupta <narinder.gupta@gmail.com>
2022-09-02 19:44:18 -0400

    commit ng 7

---
+++ Selenium_Sept_Prac/src/test/java/Day2_3/IncrementOperator.java
@@ -0,0 +1,23 @@
+package Day2_3;
+
+public class IncrementOperator {
+
+	public static void main(String[] args) {
+		// //a++;     //a=a+1;       //post increment
+		//++a; // pre increment//int res=a++;int res=++a;System.out.println(res);  // 10 System.out.println(a); 
+		
+		//Increment operator
+		
+		int a=5;
+		a=a++;//Post Increment
+				System.out.println(a);
+		a=++a;  //pre-increment
+		
+		System.out.println(a);
+	
+				
+		
+
+	}
+
+}
