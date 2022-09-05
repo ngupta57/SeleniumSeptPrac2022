1bff95963b3046fa4dbd5143a5a53ceda17cfc38
Narinder Gupta <narinder.gupta@gmail.com>
2022-09-04 20:21:57 -0400

    commit 90

--- Selenium_Sept_Prac/src/test/java/Day6/ArraySingleDimen.java
+++ Selenium_Sept_Prac/src/test/java/Day6/ArraySingleDimen.java
@@ -1,5 +1,7 @@
 package Day6;
 
+import java.util.Arrays;
+
 public class ArraySingleDimen {
 
 	public static void main(String[] args) {
@@ -15,6 +17,28 @@ public class ArraySingleDimen {
 		marks[6]=291;
 		
 		System.out.println(" THE VALUE IS  "    +  marks[1]);
+		
+		//2nd way of declaring array
+		
+		int properties []= {23,24,25,27,28};
+		System.out.println(properties.length);
+		//	//get all the values from array
+		System.out.println(Arrays.toString(properties));
+		
+		//using for loop
+		
+		for (int i=0; i <marks.length; i++) {
+			System.out.println(marks[i]);
+		}
+		// read data from array using enhanced for loop
+		
+				for(int x:marks)
+				{
+					System.out.println(x);
+				}
+				
+		
+		
 		
 		
 
