98bb43e51e862ac3158e484e79bf4331cb14c5f9
Narinder Gupta <narinder.gupta@gmail.com>
2022-09-04 20:10:54 -0400

    commit ng

---
+++ Selenium_Sept_Prac/src/test/java/Day6/ArraySingleDimen.java
@@ -0,0 +1,23 @@
+package Day6;
+
+public class ArraySingleDimen {
+
+	public static void main(String[] args) {
+		// Array Single Dimension
+		
+		int marks[]= new int [7];
+		marks[0]=6;
+		marks[1]=9;
+		marks[2]=91;
+		marks[3]=19;		
+		marks[4]=29;
+		marks[5]=49;
+		marks[6]=291;
+		
+		System.out.println(" THE VALUE IS  "    +  marks[1]);
+		
+		
+
+	}
+
+}
