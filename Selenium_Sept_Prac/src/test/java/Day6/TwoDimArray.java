0a16317d989735f8c4465235815dc16a044eae8c
Narinder Gupta <narinder.gupta@gmail.com>
2022-09-04 20:30:07 -0400

    ng commit

---
+++ Selenium_Sept_Prac/src/test/java/Day6/TwoDimArray.java
@@ -0,0 +1,21 @@
+package Day6;
+
+public class TwoDimArray {
+
+	public static void main(String[] args) {
+		// two Dim array
+		
+		int a[][]= new int[3][2];
+		a[0][0]=5;
+		a[0][1]=78;
+		a[1][0]=50;
+		a[1][1]=8;
+		a[2][0]=90;
+		a[2][1]=100;
+		
+		
+		System.out.println(a[2][1]);
+
+	}
+
+}
